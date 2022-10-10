8919ae01920a5b6f1346f03f3049f68607e23a98
David Pineda <david.pineda@deparadise.com>
2022-10-09 20:37:23 -0400

    Add another handle to accept the auth request as a JSON body

--- 1_first-go-server/server.go
+++ 1_first-go-server/server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -18,6 +20,8 @@ func HandleAuthRequest_MPform(w http.ResponseWriter, r *http.Request) {
 	// TODO - RESPONSE PREP
 	// TODO - PACKAGE/SUB-ROUTE MANAGEMENT
 
+	// TODO - IMPLEMENT FROM A POST BODY EXAMPLE
+
 	user := r.FormValue("user")
 	password := r.FormValue("password")
 	token := r.FormValue("token")
@@ -33,12 +37,30 @@ func HandleAuthRequest_MPform(w http.ResponseWriter, r *http.Request) {
 	}
 
 
-  fmt.Println(">> TEST", data)
+  fmt.Println(">> TEST INCOMING MP FORM", data)
+}
+
+func HandleAuthRequest_json(w http.ResponseWriter, r *http.Request) {
+	// TODO - same as above handle
+
+	reqBody, _ := ioutil.ReadAll(r.Body)
+
+	type AuthReq struct{
+		User string
+		Pass string
+		Token string
+	}
+	var authReq AuthReq
+
+	json.Unmarshal(reqBody, &authReq)
+	
+	fmt.Println(">> TEST INCOMING JSON", authReq)
 }
 
 func main() {
 	http.HandleFunc("/", rootHandle)
-	http.HandleFunc("/api/v0/auth", HandleAuthRequest_MPform)
+	http.HandleFunc("/api/v0/auth-mpf", HandleAuthRequest_MPform)
+	http.HandleFunc("/api/v0/auth-json", HandleAuthRequest_json)
 
 	fmt.Println("- Server is running on port :8090")
 	log.Fatal(http.ListenAndServe(":8090", nil))
